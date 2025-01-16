4828e60fb7f6d91da78187b3b61996cd0cdd0701
anvibabenko <anvibabenko@edu.hse.ru>
2025-01-16 12:34:05 +0300

    Reapply "added description to finctions"
    This reverts commit d1fed6b1c8ca083f7c1f69884f30ad2563f34b90.

--- project/db.py
+++ project/db.py
@@ -2,10 +2,24 @@ import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
 
-
 Base = declarative_base()
 
 class User(Base):
+    """
+    Модель пользователя в базе данных. Хранит информацию о пользователях, включая их данные, статистику и текущий прогресс
+    
+    Атрибуты:
+        chat_id (int): Уникальный идентификатор пользователя (основной ключ)
+        name (str): Имя пользователя
+        forename (str): Фамилия пользователя
+        group (str): Группа пользователя
+        sex (str): Пол пользователя
+        daily_streak (int): Количество дней подряд, когда пользователь выполнял задания
+        daily_date (datetime): Дата последнего выполнения задания
+        daily_total (int): Общее количество очков за выполнение заданий
+        daily_complete (bool): Статус выполнения задания на текущий день
+        games_total (int): Общее количество игр пользователя
+    """
     __tablename__ = 'users'
     chat_id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=False)
     name = db.Column(db.Text)
@@ -16,9 +30,20 @@ class User(Base):
     daily_date = db.Column(db.DateTime, default=datetime.now())
     daily_total = db.Column(db.Integer, default=0)
     daily_complete = db.Column(db.Boolean, default=0)
-    games_total = db.Column(db.Integer, default=0)          
+    games_total = db.Column(db.Integer, default=0)
 
 class History(Base):
+    """
+    Модель истории действий пользователя. Хранит записи о действиях, выполненных пользователем, для отслеживания прогресса.
+    
+    Атрибуты:
+        id (int): Уникальный идентификатор записи (основной ключ).
+        chat_id (int): Идентификатор пользователя, которому принадлежит запись.
+        name (str): Имя пользователя.
+        username (str): Имя пользователя в системе.
+        type (str): Тип действия.
+        text (str): Описание действия.
+    """
     __tablename__ = 'history'
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer, nullable=False)
@@ -28,15 +53,28 @@ class History(Base):
     text = db.Column(db.Text)
     
 def update_daily_user_stats(session, chat_id, stats):
+    """
+    Обновляет ежедневную статистику пользователя в базе данных.
+    
+    Аргументы:
+        session (Session): Сессия SQLAlchemy для выполнения операций с базой данных.
+        chat_id (int): Идентификатор пользователя, чьи данные необходимо обновить.
+        stats (int): Очки за выполненное задание, которые нужно добавить к общей сумме.
+    """
     user = session.query(User).filter(User.chat_id == chat_id).one()
     user.daily_streak += 1
     user.daily_total += stats
-    user.completed_daily = 1
+    user.daily_complete = 1
     session.add(user)
     session.commit()
-    
 
 def make_session():
+    """
+    Создает и возвращает новую сессию для взаимодействия с базой данных.
+    
+    Возвращает:
+        Session: Сессия SQLAlchemy, готовая к выполнению операций с базой данных.
+    """
     engine = db.create_engine('sqlite:///AIIEE.db')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -44,4 +82,7 @@ def make_session():
     return session
 
 if __name__ == '__main__':
+    """
+    Запускает программу и создает сессию для работы с базой данных.
+    """
     session = make_session()
--- project/main.py
+++ project/main.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -8,24 +7,41 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 from typing import Any, Awaitable, Callable, Dict
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 import config
-from handlers import router
+from handlers import router, send_notifications
 from db import make_session, User, History
 import text
 from states import check_registration_state
 
-
 class DatabaseMiddleware(BaseMiddleware):
+    """
+    Промежуточное ПО для работы с базой данных. Сохраняет информацию о действиях пользователя в базе данных
+    
+    Аргументы:
+        session (Session): Сессия для работы с базой данных
+        
+    Методы:
+        __call__(handler, event, data): Основной метод, который перехватывает запросы, сохраняет данные в историю
+        и проверяет регистрацию пользователя
+    """
     def __init__(self, session) -> None:
         self.session = session
 
-    async def __call__(
-                       self, 
-                       handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-                       event: TelegramObject, 
-                       data: Dict[str, Any]) -> Any:
+    async def __call__(self, handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]], event: TelegramObject, data: Dict[str, Any]) -> Any:
+        """
+        Перехватывает каждый запрос и добавляет информацию о действии пользователя в базу данных
         
+        Аргументы:
+            handler (Callable): Основной обработчик события
+            event (TelegramObject): Событие, которое вызвало обработку (например, сообщение от пользователя)
+            data (Dict): Дополнительные данные, которые могут быть использованы в обработке
+            
+        Возвращает:
+            Любой результат выполнения обработчика
+        """
         chat_id = data['event_context'].chat.id
         session = self.session
         current_state = await data.get('state').get_state()
@@ -43,6 +59,7 @@ class DatabaseMiddleware(BaseMiddleware):
                           username=data['event_context'].chat.username, text=text_, type=type)
         session.add(history)
         session.commit()
+        
         if session.query(User).filter(User.chat_id == chat_id).count() > 0 or \
            text_ in ['/register', '/stop'] or check_registration_state(current_state):
             with session as session:
@@ -50,18 +67,36 @@ class DatabaseMiddleware(BaseMiddleware):
                 return await handler(event, data)
         else:
             await bot.send_message(chat_id=chat_id, text=text.signup.format(name=msg.from_user.full_name))
-            
-            
+
 bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
 dp = Dispatcher(storage=MemoryStorage())
+
 dp.include_router(router)
+
 dp.update.middleware(DatabaseMiddleware(session=make_session()))
 
 async def main():
+    """
+    Основная асинхронная функция для запуска бота, планировщика задач и обработки обновлений
+    """
+    scheduler = AsyncIOScheduler()
+
+    scheduler.add_job(
+        send_notifications,
+        CronTrigger(hour=21, minute=53),
+        kwargs={"bot": bot, "session": make_session()}
+    )       
+
+    scheduler.start()
+
+    logging.info("bot is starting...")
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-
+    logging.info("bot has stopped.")
 
 if __name__ == "__main__":
+    """
+    Основная точка входа в программу. Настроена запись логов и запуск основного процесса
+    """
     logging.basicConfig(level=logging.INFO, filename="bot.log", filemode='a', format="%(asctime)s %(levelname)s %(message)s")
     asyncio.run(main())
